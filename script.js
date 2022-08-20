08e4ae5a2770825fa6ba97fbed91725119495feb
Fernanda Avalos <fernanda.avalos2002@gmail.com>
2022-08-19 19:56:00 -0500

    Actualizacion del 19 de agosto

---
+++ script.js
@@ -0,0 +1,23 @@
+//Global e inneficiente, fácil de mutar
+var var1 = 'var1'
+//Constante, no cambiar valor
+const const1 = 'const1'
+//Variable
+let let1 = 'let1'
+
+console.log('let1', let1)
+console.log(let1)
+console.log('const1, let1', const1, let1)
+
+console.info
+console.warn
+
+function name(params) {
+
+}
+
+const myFunction = (txt) => {
+    console.log(txt, 'from func')
+}
+
+myFunction('Hola')
