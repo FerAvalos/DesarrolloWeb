ddd4c3d84e81347aeb06df9d4cc5f5a287879e32
Fernanda Avalos <fernanda.avalos2002@gmail.com>
2022-10-16 17:20:00 -0500

    Tarea 16 de octubre

--- movies-app/src/Hooks/useFetchTrending.js
+++ movies-app/src/Hooks/useFetchTrending.js
@@ -1,18 +1,21 @@
 import { useEffect, useState } from "react"
 import { getTrending } from "../Helpers/getTrending";
 
-export const useFetchTrending = () => {
+export const useFetchTrending = (selectedOption) => {
     const [movies, setMovies] = useState([]);
     const [itsLoading, setIsLoading] = useState(true); //Está cargando
-    const getTrendingMovies = async (media_type, time_window) => {
-        const results = await getTrending(media_type, time_window)
+    const getTrendingMovies = async (selectedOption, time_window) => {
+        console.log("Hola " + selectedOption);
+        const results = await getTrending(selectedOption, time_window)
         setMovies(results)
         setIsLoading(false)
     }
-    useEffect(() => {
-        getTrendingMovies()
-    }, [])
 
+    useEffect(() => {
+        getTrendingMovies(selectedOption)
+        console.log("Probando")
+    }, [selectedOption])
+    
     return {
         movies,
         itsLoading
--- movies-app/src/Pages/Trending.js
+++ movies-app/src/Pages/Trending.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import { MovieCard } from '../Components/MovieCard';
 import { useFetchTrending } from '../Hooks/useFetchTrending'
+import { useState } from "react"
+import Select from 'react-select';
+
+const Opciones = [
+  {value: 'all', label: 'All'},
+  {value: 'movie', label: 'Movie'},
+  {value: 'tv', label: 'TV'},
+  {value: 'person', label: 'Person'},
+
+]
 
 export const Trending = () => {
 
   const { movies } = useFetchTrending();
-
+  const [selectedOption, setSelectedOption] = useState("")
+  useFetchTrending(selectedOption)
+  const handleChange = (value) => {
+    console.log(value.value);
+    const selectedOption = value.value
+    setSelectedOption(selectedOption)
+  }
+  
   return (
     <div className='container row row-cols-1 row-cols-md-3 g-3'>
+      <Select 
+        options={Opciones}
+        onChange={handleChange}
+      />
       { movies.map((movie) => 
         <MovieCard 
           key={movie.id}
